978bdafb5ee431f4c5ba65d9ee9196b36c359279
chibaf <chibaf@mac.com>
2023-12-13 21:15:24 +0900

    Add files via upload

---
+++ read_s2.py
@@ -0,0 +1,12 @@
+import serial, sys
+
+strPort = sys.argv[1]
+
+ser=serial.Serial(strPort, sys.argv[2])
+
+print("connected to: " + ser.portstr)
+
+while True:
+  line = ser.readline()
+  line2=line.decode('utf-8')
+  print(line2)
